bcab7e75210e89ff394e53f779eba1e172807f98
fazzaamiarso <fazzaamiarso@gmail.com>
2023-01-26 19:32:21 +0700

    wrapping up submission-1

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -8,6 +8,9 @@ window.addEventListener("DOMContentLoaded", () => {
   const catalogList = document.querySelector("#catalog-list");
 
   catalog.restaurants.forEach((r) => {
+    const formattedRating = new Intl.NumberFormat("en-US", { minimumFractionDigits: 1 }).format(
+      r.rating
+    );
     const item = document.createElement("li");
     item.classList.add("catalog__card");
     item.innerHTML = `
@@ -22,7 +25,7 @@ window.addEventListener("DOMContentLoaded", () => {
           <svg class="catalog__star" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
             <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z" clip-rule="evenodd" />
           </svg>
-          ${new Intl.NumberFormat("en-US", { minimumFractionDigits: 1 }).format(r.rating)}
+          ${formattedRating}
         </span>
             -
           <span>${r.city}</span>

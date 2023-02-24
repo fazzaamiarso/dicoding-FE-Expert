f91d5ed101cf1c53fd8a1ee5d2c3a4a9a00088cb
fazzaamiarso <fazzaamiarso@gmail.com>
2023-02-25 00:07:13 +0700

    add split chunking

--- webpack.prod.ts
+++ webpack.prod.ts
@@ -1,7 +1,6 @@
 import common from "./webpack.common";
 import { merge } from "webpack-merge";
 import WorkboxWebpackPlugin from "workbox-webpack-plugin";
-import ImageMinimizerPlugin from "image-minimizer-webpack-plugin";
 import path from "path";
 
 export default merge(common, {
@@ -18,40 +17,29 @@ export default merge(common, {
       },
     ],
   },
-  // optimization: {
-  //   minimizer: [
-  //     new ImageMinimizerPlugin({
-  //       minimizer: {
-  //         implementation: ImageMinimizerPlugin.sharpMinify,
-  //         options: {
-  //           encodeOptions: {
-  //             jpeg: {
-  //               // https://sharp.pixelplumbing.com/api-output#jpeg
-  //               quality: 60,
-  //               progressive: true,
-  //             },
-  //             webp: {
-  //               // https://sharp.pixelplumbing.com/api-output#webp
-  //               lossless: true,
-  //             },
-  //             avif: {
-  //               // https://sharp.pixelplumbing.com/api-output#avif
-  //               lossless: true,
-  //             },
-
-  //             // png by default sets the quality to 100%, which is same as lossless
-  //             // https://sharp.pixelplumbing.com/api-output#png
-  //             png: {},
-
-  //             // gif does not support lossless compression at all
-  //             // https://sharp.pixelplumbing.com/api-output#gif
-  //             gif: {},
-  //           },
-  //         },
-  //       },
-  //     }),
-  //   ],
-  // },
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+      minSize: 20000,
+      maxSize: 70000,
+      minChunks: 1,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      automaticNameDelimiter: "~",
+      enforceSizeThreshold: 50000,
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
+    },
+  },
   plugins: [
     new WorkboxWebpackPlugin.InjectManifest({
       swSrc: path.resolve(__dirname, "src/utils/sw/sw.ts"),
